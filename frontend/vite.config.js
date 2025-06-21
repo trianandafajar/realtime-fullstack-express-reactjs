78d14ce4798320d1067a5c5386ed1d61a9bf99ac
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-21 09:38:55 +0700

    Update Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,47 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    cors: true,
+  },
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          utils: ['axios', 'swr'],
+          ui: ['sweetalert2'],
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', 'axios', 'swr'],
+  },
+  css: {
+    postcss: {
+      plugins: [
+        require('autoprefixer'),
+        require('tailwindcss'),
+      ],
+    },
+  },
 })
